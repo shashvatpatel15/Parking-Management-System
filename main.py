cbaac6551073e2f509b4bbd3b25d4755145289b1
Shashvat <shashvatpatel15@gmail.com>
2025-12-30 10:36:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ import bcrypt
 def main():
     db.create_table_vehicles()
     db.create_table_admin()
+    # while running this program for the first time 
+    # make sure to add this below admin in db 
+
     # password="Shashvat@123"
     # hashed_password = db.hash_password(password)
     # db.insert_admin("spatel",hashed_password)
